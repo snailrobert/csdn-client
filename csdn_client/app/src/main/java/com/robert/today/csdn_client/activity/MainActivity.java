43a77345f4307073a47e7d3357222671da975b7d
chenjun <chenjun0526@126.com>
2015-01-05 15:25:00 +0800

    初始化
    初始化項目。1.添加gradle支持 2.添加ActionBarSherlock和support-v4�
    3.值得注意：在gradle支持中要注意在application中的repositories的配置，有些項目是mavenCentral()，即maven中央库，还有一些是jcenter()，也是maven的一种，但是他们compile的语法不同。现在Android
    Studio默认是jcenter()，最好改成mavenCentral()

---
+++ csdn_client/app/src/main/java/com/robert/today/csdn_client/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.robert.today.csdn_client.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.robert.today.csdn_client.R;
+
+/**
+ * Created by chenjun06 on 2015/1/5.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
